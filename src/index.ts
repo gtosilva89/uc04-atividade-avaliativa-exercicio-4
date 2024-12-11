0e00b7da8067ba5d13d3f35257f8ac0e940aa052
Gustavo Silveira Silva <silva.gustavo@hotmail.com.br>
2024-12-11 19:28:07 -0300

    exercicio

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
-console.log("Olá mundo");
+// O jogo da memória é um clássico jogo formado por peças que apresentam uma figura em um dos lados.
+// Cada figura se repete em duas peças diferentes. Para começar o jogo, as peças são postas com as figuras voltadas para baixo, para que não possam ser vistas.
+// Cada participante deve, na sua vez, virar duas peças, caso as figuras sejam iguais, o participante deve recolher consigo esse par e jogar novamente.
+// Se forem peças diferentes, estas devem ser viradas novamente, e sendo passada a vez ao participante seguinte. Ganha o jogo quem tiver mais pares no final do jogo.
+// Nesse exercício, será uma versão simplificada do jogo, onde o computador deve descobrir os resultados das combinações.
+// Dado o trecho de código para geração de um jogo da memória com 8 palavras, as quais serão armazenadas em um vetor de 16 posições dispostas aleatoriamente,
+// crie um código para resolver e imprimir o resultado das posições com as palavras iguais, por exemplo: vetor[4] é igual ao vetor[6]; vetor [2] é igual ao vetor [3].
